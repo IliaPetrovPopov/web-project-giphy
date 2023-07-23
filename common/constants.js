1fa910b48863c34d17e24d54f28aa336009f6932
Ilia Popov <ilia_popov02@abv.bg>
2023-07-23 10:06:14 +0300

    Add common folder components

---
+++ common/constants.js
@@ -0,0 +1,21 @@
+/**
+ * A JS File to store all constants for the webpage functionality
+ */
+
+// API keys for development
+export const API_KEY_SLAVENA = 'Ij7hN73HsD3Gk6zYBknCnCIlcR70hegJ';
+export const API_KEY_VALENTIN = 'hIoUFFNtXcIVQ5lOJQAEkldNrQvAPAbn';
+export const API_KEY_ILIA = 'qJGLIfEGRqxDOYoCddo5KjrMk5ZeDyPj';
+
+// limit parameters for service requests
+export const TRENDING_LIMIT = 28;
+export const SEARCH_LIMIT = 20;
+
+// icons
+export const FULL_HEART = '❤';
+export const EMPTY_HEART = '♡';
+export const INFO = 'ℹ';
+
+// localStorage keys
+export const FAVORITE_GIFS = 'favoriteGIFsIDs';
+export const UPLOADED_GIFS = 'uploaded-gifs';
---
+++ common/elements-ids.js
@@ -0,0 +1,18 @@
+/**
+ * A JS file to store variables for the HTML elements on the webpage
+ */
+
+export const titleID = 'page-title';
+
+export const trendingTabID = 'trending-tab';
+export const favoritesTabID = 'favorites-tab';
+export const aboutTabID = 'about-tab';
+export const uploadedTabID = 'uploaded-gifs-tab';
+
+export const searchBoxID = 'search-box';
+export const searchInputID = 'search-input';
+
+export const uploadButtonID = 'upload-button';
+export const uploadBoxID = 'file-input';
+
+export const gifListID = 'gif-list';
